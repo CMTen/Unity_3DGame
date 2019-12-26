c06d3fdabdaebc13f5fbe8c1fa572d5b28be09e9
CMTen <57666274+CMTen@users.noreply.github.com>
2019-12-26 21:35:33 +0800

    鏡頭追蹤

--- Unity_ZL_3D_Game/Assets/Script/Player.cs
+++ Unity_ZL_3D_Game/Assets/Script/Player.cs
@@ -8,14 +8,14 @@ public class Player : MonoBehaviour
     private Rigidbody rig;
     private Joystick js;
     private Animator ani;
-    private Transform tra;
+    private Transform target;
 
     private void Start()
     {
         rig = GetComponent<Rigidbody>();
         ani = GetComponent<Animator>();
-        tra = GetComponent<Transform>();
         js = GameObject.Find("虛擬搖桿").GetComponent<FixedJoystick>();
+        target = GameObject.Find("目標").transform;
     }
 
     private void FixedUpdate()
@@ -25,11 +25,19 @@ public class Player : MonoBehaviour
 
     private void move()
     {
-        float v = js.Vertical;
-        float h = js.Horizontal;
+        float v = -js.Vertical;
+        float h = -js.Horizontal;
 
-        rig.AddForce(-h * speed, 0, -v * speed);
+        rig.AddForce(h * speed, 0, v * speed);
         ani.SetBool("跑步開關", h != 0 || v != 0);
-   
+
+        Vector3 pos = transform.position;
+        target.position = new Vector3(pos.x + h, 0.01f, pos.z + v);
+
+        //transform.LookAt(target);  // 這會吃土
+
+        Vector3 posTarget = new Vector3(target.position.x, transform.position.y, target.position.z);
+        transform.LookAt(posTarget);
+
     }
 }
